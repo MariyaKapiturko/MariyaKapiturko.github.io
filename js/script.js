ceea2e47c018ce1716f0d19ac7771e7e121edef2
MariyaKapiturko <mariya.kapiturko@mail.ru>
2019-09-10 00:49:05 +0300

    Adaptive

---
+++ js/script.js
@@ -0,0 +1,28 @@
+$(".review-cards").slick({
+    autoplay: true,
+    autoplaySpeed: 2000,
+  dots: true,
+  infinite: true,
+  responsive: [
+    {
+      breakpoint: 990,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+      }
+    }
+  ]
+});
